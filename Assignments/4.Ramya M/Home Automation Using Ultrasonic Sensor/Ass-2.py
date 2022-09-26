c8344c9b785c2a827df479a3297b509a9df62694
Ramyamaha14 <113895621+Ramyamaha14@users.noreply.github.com>
2022-09-26 15:59:43 +0530

    Create Ass-2.py

---
+++ Assignments/4.Ramya M/Home Automation Using Ultrasonic Sensor/Ass-2.py
@@ -0,0 +1,14 @@
+import random
+a=random.randint(1,190)  #temperature
+b=random.randint(1,150)   #humiditY
+print(a)
+print(b)
+if a>80:
+    if b>80:
+        print("Alram ON")
+        print ("hazard predicted")
+    else:
+        print("Alram ON")
+        print ("high temp")
+else:
+    print ("Alarm OFF")
